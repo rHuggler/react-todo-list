6a3cb82149ee10bee1373602fb9c40dcbef9f978
Ronald Huggler <ronald@huggler.com.br>
2024-07-26 16:59:17 -0300

    Remove default files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { App } from "./App.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: <Vite default>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
