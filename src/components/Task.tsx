da164ac63fc84a03efe52a1eda1ed8767243e5ed
Ronald Huggler <ronald@huggler.com.br>
2024-07-27 19:41:05 -0300

    Create task done state

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,26 @@
 import { Trash } from "@phosphor-icons/react";
+import type React from "react";
+import { useState } from "react";
 import styles from "./Task.module.css";
 
 export function Task() {
+	const [taskStatus, setTaskStatus] = useState(false);
+
+	function handleTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+		setTaskStatus(e.target.checked);
+	}
+
 	return (
 		<div className={styles.task}>
 			<div className={styles.checkboxContainer}>
-				<input type="checkbox" name="status" id="taskId" />
+				<input
+					type="checkbox"
+					name="status"
+					id="taskId"
+					onChange={handleTaskStatus}
+				/>
 			</div>
-			<p className={styles.doneItem}>
+			<p className={taskStatus ? styles.doneItem : ""}>
 				Integer urna interdum massa libero auctor neque turpis turpis semper.
 				Duis vel sed fames integer.
 			</p>
